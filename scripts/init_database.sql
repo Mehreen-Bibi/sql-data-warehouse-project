b6d920fcb25b640df1ff771c0442adac3d946c2c
Mehreen Bibi <mahifatima413@gmail.com>
2026-05-19 17:40:26 +0500

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,40 @@
+/*
+==============================
+Create Database and Schemas
+==============================
+Script purpose:
+	This script create a new database name 'DataWarehouse' after checking if it is already exists.
+	If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas
+	within the database: 'bronze', 'silver', 'gold'.
+
+WARNINGS:
+	Running this script will drop the entire 'Datawarehouse' database if it exists.
+	All data in the database will be permanently deleted. Proceed with caution
+	and ensure you have proper backups before running this script.
+*/
+Use master;
+GO
+
+-- Drop and recreate the 'DataWarehouse' database.
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
